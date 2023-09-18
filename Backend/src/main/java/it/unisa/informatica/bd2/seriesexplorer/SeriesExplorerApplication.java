378ee1beef3a99e80dff3eeeaa5ec8e530093978
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2023-09-18 14:43:50 +0200

    [BE] Configuration
    Project configuration

---
+++ Backend/src/main/java/it/unisa/informatica/bd2/seriesexplorer/SeriesExplorerApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.informatica.bd2.seriesexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesExplorerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/it/unisa/informatica/bd2/seriesexplorer/SeriesExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.informatica.bd2.seriesexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
